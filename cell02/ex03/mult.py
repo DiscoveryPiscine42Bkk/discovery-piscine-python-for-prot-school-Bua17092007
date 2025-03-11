26682c1238f04c23885ce3dbd054366357e8ab66
Bua17092007 <40320@prot.ac.th>
2025-03-11 10:51:33 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+number_3 = int(input("enter the first number"))
+number_5 = int(input("enter the second number"))
+
+result = number_3 * number_5
+print(number_3,"x",number_5,"=",result)
+
+if result ==0 :
+    print("the result is both side negative and positive")
+
+    if result > 0 :
+        print ("the result is positive")
+
+if result < 0 :
+    print("the result is negative ") 
