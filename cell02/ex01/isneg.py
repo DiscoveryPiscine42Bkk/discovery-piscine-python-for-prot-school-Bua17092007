0a4b7227db791b5896e2061f7b24e37c7caa77fd
Bua17092007 <40320@prot.ac.th>
2025-03-11 10:06:29 +0700

    Add files via upload

---
+++ cell02/ex01/isneg.py
@@ -0,0 +1,9 @@
+number = int(input("please enter"))
+if nunber == 0 :
+    print("this number is both side negative and positive")
+
+if number > 0 :
+    print("this number positive")
+
+if numeber < 0 :
+    print("this number negative")
