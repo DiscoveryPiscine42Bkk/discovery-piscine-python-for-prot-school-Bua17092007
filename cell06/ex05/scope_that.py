ebd31a39d9b78bc1738cf60cdf8c11320802edee
Bua17092007 <40320@prot.ac.th>
2025-03-13 10:22:03 +0700

    Add files via upload

---
+++ cell06/ex05/scope_that.py
@@ -0,0 +1,9 @@
+def add_one(number):
+    return number + 1
+def main():
+    my_ver = 5
+    print(f"Initial value of my_ver: {my_var}")
+    my_var = madd_one(my_var)
+    print(f"Updatad value of my_var after add-one: {my_var}")
+if __name__ == " __main__ ":
+    main()
